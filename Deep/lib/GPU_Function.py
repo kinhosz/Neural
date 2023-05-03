1c072433dc3989c1a55fa53e02fc0b4fec995fe5
kinhosz <jcsc@cin.ufpe.br>
2023-05-02 22:15:27 -0300

    test: add correctness test for gpu lib

--- Deep/lib/GPU_Function.py
+++ Deep/lib/GPU_Function.py
@@ -141,8 +141,7 @@ def dotMatrix(arr, A, B):
 	
 	cuda.atomic.add(arr, (x, z), A[x, y] * B[y, z])
 
-###################
-def dotMatrix_derivate_cpu(x,w,alpha):
+def dotMatrix_derivate_cpu(w, alpha):
 	return alpha.dot(w.transpose())
 
 @cuda.jit
@@ -174,10 +173,6 @@ def updateWeights(w, eta, nabla):
 
 	if x < w.shape[0] and y < w.shape[1]:
 		w[x, y] -= eta[0] * nabla[x, y]
-
-'''
-Test Correctness
-'''
 
 def ceil(A, B):
 	return (A + B - 1) // B
@@ -522,11 +517,77 @@ def dotMatrix_test():
 	
 	return True
 
+def dotMatrix_derivate_test():
+	LEN_ARRAY1 = 200
+	LEN_ARRAY2 = 100
+
+	w_host = np.random.randn(LEN_ARRAY1, LEN_ARRAY2)
+	alpha_host = np.random.randn(1, LEN_ARRAY2)
+	aux = np.random.randn(1, LEN_ARRAY1)
+	w_device = cuda.to_device(w_host)
+	alpha_device = cuda.to_device(alpha_host)
+	ans_device = cuda.to_device(aux)
+
+	ans_cpu = dotMatrix_derivate_cpu(w_host, alpha_host)
+	memset2[kernelConfig2D(1, LEN_ARRAY1)](ans_device)
+	dotMatrix_derivate[kernelConfig3D(1, LEN_ARRAY1, LEN_ARRAY2)](ans_device, w_device, alpha_device)
+	cuda.synchronize()
+	ans_gpu = ans_device.copy_to_host()
+
+	for i in range(LEN_ARRAY1):
+		if abs(ans_cpu[0, i] - ans_gpu[0, i]) > EPS:
+			return False
+	
+	return True
+
+def transposeDot_test():
+	LEN_ARRAY1 = 100
+	LEN_ARRAY2 = 200
+
+	x_host = np.random.randn(1, LEN_ARRAY1)
+	derror_host = np.random.randn(1, LEN_ARRAY2)
+	x_device = cuda.to_device(x_host)
+	derror_device = cuda.to_device(derror_host)
+	ans_cpu = transposeDot_cpu(x_host, derror_host)
+	ans = np.random.randn(LEN_ARRAY1, LEN_ARRAY2)
+	ans_device = cuda.to_device(ans)
+	transposeDot[kernelConfig2D(LEN_ARRAY1, LEN_ARRAY2)](ans_device, x_device, derror_device)
+	ans_gpu = ans_device.copy_to_host()
+
+	for i in range(LEN_ARRAY1):
+		for j in range(LEN_ARRAY2):
+			if abs(ans_gpu[i, j] - ans_cpu[i, j]) > EPS:
+				return False
+	
+	return True
+
+def updateWeights_test():
+	LEN_ARRAY1 = 100
+	LEN_ARRAY2 = 200
+
+	w_host = np.random.randn(LEN_ARRAY1, LEN_ARRAY2)
+	eta_host = np.random.randn(1)
+	nabla_host = np.random.randn(LEN_ARRAY1, LEN_ARRAY2)
+	w_device = cuda.to_device(w_host)
+	eta_device = cuda.to_device(eta_host)
+	nabla_device = cuda.to_device(nabla_host)
+	ans_cpu = updateWeights_cpu(w_host, eta_host, nabla_host)
+	updateWeights[kernelConfig2D(LEN_ARRAY1, LEN_ARRAY2)](w_device, eta_device, nabla_device)
+	ans_gpu = w_device.copy_to_host()
+
+	for i in range(LEN_ARRAY1):
+		for j in range(LEN_ARRAY2):
+			if abs(ans_cpu[i, j] - ans_gpu[i, j]) > EPS:
+				return False
+	
+	return True
+
 def test():
 	init()
 	tests = [memset_test, memset2_test, mse_test, mse_derivate_test, softmax_test,
 	  		softmax_derivate_test, sigmoid2_test, sigmoid2_derivate_test, copy_test,
-			dotMatrix_loop_test, dotMatrix_test]
+			dotMatrix_loop_test, dotMatrix_test, dotMatrix_derivate_test,
+			transposeDot_test, updateWeights_test]
 
 	failed_tests = []
 
