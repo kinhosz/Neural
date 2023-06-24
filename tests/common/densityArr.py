f3676b7edfc47b60423f49b1f96a213178406cd8
kinhosz <jcsc@cin.ufpe.br>
2023-06-24 06:01:41 -0300

    test: learn_rate

---
+++ tests/common/densityArr.py
@@ -0,0 +1,4 @@
+def densityArr(idx: int, len: int):
+    arr = [0.0] * len
+    arr[idx] = 1.0
+    return arr
---
+++ tests/common/read_image_files.py
@@ -0,0 +1,20 @@
+def read_image_files(url):
+	f = open(url,"rb")
+	magic_number = int.from_bytes(f.read(4), byteorder="big")
+	number_of_images = int.from_bytes(f.read(4), byteorder="big")
+	rows = int.from_bytes(f.read(4),byteorder="big")
+	columns = int.from_bytes(f.read(4),byteorder="big")
+
+	images = []
+
+	for img in range(number_of_images):
+		image = []
+		for i in range(rows):
+			for j in range(columns):
+				pixel = int.from_bytes(f.read(1),byteorder="big",signed=False)
+				image.append(pixel)
+		images.append(image)
+
+	f.close()
+
+	return images
