921b93f9ce17b6ed88557c230a2299a7102704ff
kinhosz <jcsc@odoo.com>
2024-09-23 13:50:39 +0200

    [FEAT] convNeural: add public methods & helpers
    This commit adds the following public methods:
    * send
    * learn
    * cost
    
    And, its private implementations:
    * _feed_forward
    * _backpropagation
    * _cost
    
    For to do:
    * export
    
    It adds the helper calculate_pooling_layers

--- Kinho/__init__.py
+++ Kinho/__init__.py
@@ -1,6 +1,8 @@
+from .convNeural import ConvNeural
 from .neural import Neural
 
 from .tensor import Tensor
+from .helper import *
 
 __author__ = 'Kinhosz'
 __email__ = 'scruz.josecarlos@gmail.com'
